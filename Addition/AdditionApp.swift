e53f79c51c95081cd01f08ce80642263de0bc86a
Raghavendra Reddy <s542272@nwmissouri.edu>
2021-03-23 09:55:07 -0500

    Initial Commit

---
+++ Addition/AdditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdditionApp.swift
+//  Addition
+//
+//  Created by sai kiran reddy on 23/03/21.
+//
+
+import SwiftUI
+
+@main
+struct AdditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
